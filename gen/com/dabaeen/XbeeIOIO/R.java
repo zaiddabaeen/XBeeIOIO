0fe622050c5134328d16eb4aedc791d1260e3651
zaiddabaeen <zaid@redtroops.com>
2014-12-29 22:57:53 +0200

    New layout for configuration activity

--- gen/com/dabaeen/XbeeIOIO/R.java
+++ gen/com/dabaeen/XbeeIOIO/R.java
@@ -4,48 +4,21 @@
  * aapt tool from the resource data it found.  It
  * should not be modified by hand.
  */
-
 package com.dabaeen.XbeeIOIO;
 
 public final class R {
-    public static final class attr {
-    }
-    public static final class drawable {
-        public static int ic_launcher=0x7f020000;
-    }
-    public static final class string {
-        public static int app_name=0x7f040000;
-    }
-    public static final class style {
-        /** 
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
-    
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
-
-        Base application theme for API 11+. This theme completely replaces
-        AppBaseTheme from res/values/styles.xml on API 11+ devices.
-    
- API 11 theme customizations can go here. 
-
-        Base application theme for API 14+. This theme completely replaces
-        AppBaseTheme from BOTH res/values/styles.xml and
-        res/values-v11/styles.xml on API 14+ devices.
-    
- API 14 theme customizations can go here. 
-         */
-        public static int AppBaseTheme=0x7f050000;
-        /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
-         */
-        public static int AppTheme=0x7f050001;
-    }
-    public static final class xml {
-        public static int accessory_filter=0x7f030000;
-        public static int device_filter=0x7f030001;
-    }
+	public static final class drawable {
+		public static final int ic_launcher = 0x7f020003;
+	}
+	public static final class string {
+		public static final int app_name = 0x7f060000;
+	}
+	public static final class style {
+		public static final int AppBaseTheme = 0x7f070000;
+		public static final int AppTheme = 0x7f070001;
+	}
+	public static final class xml {
+		public static final int accessory_filter = 0x7f050000;
+		public static final int device_filter = 0x7f050001;
+	}
 }
